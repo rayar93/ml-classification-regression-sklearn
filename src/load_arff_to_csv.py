3e9e84b71da5c800445eca592fe75d2a83cd8d16
rayar93 <156842273+rayar93@users.noreply.github.com>
2026-02-19 07:00:46 -0500

    Data wrangling notebook skeleton

--- src/load_arff_to_csv.py
+++
@@ -1,7 +0,0 @@
-from scipy.io import arff
-import pandas as pd
-
-data, meta = arff.loadarff("data/raw/CEE_DATA.arff")
-df = pd.DataFrame(data)
-df.to_csv("data/raw/CEE_DATA.csv", index=False)
-print(df.shape)
